ad2dd46cf2bdd5369a3636c2bac63559e4062b4d
Tech4Humanity <62584578+TML-4PM@users.noreply.github.com>
2026-05-16 07:36:25 +1000

    ccp(cdk): canonical constants (account/region/refs)

---
+++ connector-control-plane/cdk/lib/constants.ts
@@ -0,0 +1,9 @@
+export const CCP = {
+  PRODUCT: 'connector-control-plane',
+  OWNER: 'tech-4-humanity',
+  COST_CENTER: 'T4H-CCP',
+  REGION: 'ap-southeast-2',
+  ACCOUNT: '140548542136',
+  SUPABASE_PROJECT_REF: 'lzfgigiyqpuuxslsygjt',
+  BRIDGE_URL: 'https://zdgnab3py0.execute-api.ap-southeast-2.amazonaws.com',
+} as const;
